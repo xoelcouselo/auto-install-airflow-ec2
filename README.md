ae9cd451c0599d204f0a2fa08b6ab87ce53d9742
Xoel Couselo <45366808+xoelcouselo@users.noreply.github.com>
2022-09-12 22:42:15 +0200

    Code is okey we iniciate the alpha testing

---
+++ README.md
@@ -0,0 +1,10 @@
+# Airflow AWS EC2 Auto-Instaler
+
+This script (main.sh) create a basic install of AirFlow whit Anaconda Environment:
+
+- Install of Anaconda whit Python3.9
+- Create a Airflow enviroment (named: airflow)
+- Install Airflow
+- Run Airflow DDBB
+- Start Airflow UI
+- Start a Airflow Schduler
---
+++ main.bash
@@ -0,0 +1,61 @@
+echo "
+   ███████╗ ██████╗██████╗  ─────▄█▀█▄──▄███▄────❤ █████╗ ██╗██████╗ ███████╗██╗      ██████╗ ██╗    ██╗
+   ██╔════╝██╔════╝╚════██╗ ────▐█░██████████▌──── ██╔══██╗██║██╔══██╗██╔════╝██║     ██╔═══██╗██║    ██║
+   █████╗  ██║      █████╔╝ ─────██▒█████████───── ███████║██║██████╔╝█████╗  ██║     ██║   ██║██║ █╗ ██║
+   ██╔══╝  ██║     ██╔═══╝  ──────▀████████▀────── ██╔══██║██║██╔══██╗██╔══╝  ██║     ██║   ██║██║███╗██║
+   ███████╗╚██████╗███████╗ ─────────▀██▀───────── ██║  ██║██║██║  ██║██║     ███████╗╚██████╔╝╚███╔███╔╝
+   ╚══════╝ ╚═════╝╚══════╝                        ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝ ╚═════╝  ╚══╝╚══╝ 
+                                                                                                         
+   ### The install has been started ###                                                                   
+"
+
+# Incial of install
+cd /tmp
+
+# Install Anaconda3 (2022.05) and comprobation of Hash (sha256sum)
+echo "   ### The install Anaconda3 ###                                                                   "
+sleep 1
+curl -O https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+
+sha256sum Anaconda3-2022.05-Linux-x86_64.sh
+
+bash Anaconda3-2022.05-Linux-x86_64.sh
+
+# Create a python virtual enviroment
+echo "   ### Create a enviroment  called: airflow (Python=3.9) ###                                       "
+sleep 1
+conda create -n airflow python=3.9 anaconda
+
+# Enter to the airflow enviroment
+source activate airflow
+
+# Install of Airflow
+echo "   ### Create a enviroment  called: airflow (Python=3.9) ###                                       "
+sleep 1
+
+pip install apache-airflow
+
+
+# Start DDBB of airflow
+echo "   ### Starting DDBB of airflow ###                                        "
+sleep 1
+
+airflow initdb
+
+echo "   ### Starting Web UI of airflow ###                                        "
+sleep 1
+
+airflow web server -p 8070
+
+airflow web server -p 8070 -D
+echo "   ### Web UI of airflow go to: http://localhost:8070 ###                         "
+echo "   ### Please remenber go to Inbound rules and edit port 8070 of this instance ###"
+echo "   ### Recomend 0.0.0.0/0 ###                                                     "
+echo "   ### Other for more Security Options ###                                        "
+sleep 3
+
+source activate airflow
+echo "   ### Starting Scheduler of airflow ###                                        "
+sleep 1
+airflow scheduler
+
